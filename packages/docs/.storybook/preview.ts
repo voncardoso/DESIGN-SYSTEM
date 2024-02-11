2a708048b52ba188d67af2fbe01d424d2f9e8c7e
Von Harrison Tadaiesky <vonharrison26@gmail.com>
2024-02-11 20:23:02 -0300

    init

---
+++ packages/docs/.storybook/preview.ts
@@ -0,0 +1,19 @@
+
+import type { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    backgrounds: {
+      default: "dark",
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
