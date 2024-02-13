c9eac5eae660dca136c8b14e5f271a215685eb3f
Von Harrison Tadaiesky <vonharrison26@gmail.com>
2024-02-13 16:35:06 -0300

    remove adjustemnet config storybook

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -25,11 +25,6 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
-  viteFinal: (config, { configType }) => {
-    if(configType === 'PRODUCTION') {
-      config.base = '/DESIGN-SYSTEM/'
-    }
-    return config
-  }
+
 };
 export default config;
