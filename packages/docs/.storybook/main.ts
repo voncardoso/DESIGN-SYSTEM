aea4a17933e4a90e58a2dc14ea93932d2bacf935
Von Harrison Tadaiesky <vonharrison26@gmail.com>
2024-02-13 16:31:18 -0300

    adjustemnet config storybook

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -25,5 +25,11 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, { configType }) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/DESIGN-SYSTEM/'
+    }
+    return config
+  }
 };
 export default config;
